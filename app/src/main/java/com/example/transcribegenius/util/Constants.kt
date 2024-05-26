132357e38068d6ca965cdbd8e07f1ce501a38626
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-05-26 12:44:56 +0530

    hiding api keys

--- app/src/main/java/com/example/transcribegenius/util/Constants.kt
+++ app/src/main/java/com/example/transcribegenius/util/Constants.kt
@@ -1,4 +1,7 @@
 package com.example.transcribegenius.util
 
+const val X_RapidAPI_Key = "X_RapidAPI_Key"
 
+const val X_RapidAPI_Host = "X_RapidAPI_Host"
 
+const val Authorization = "OPEN_AI_API_KEY"

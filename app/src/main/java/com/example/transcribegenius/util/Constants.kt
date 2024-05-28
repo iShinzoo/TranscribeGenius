0fadf0b5a860781ca819e4f00b88163c1e4ef2b2
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-05-28 11:02:23 +0530

    hiding api keys

--- app/src/main/java/com/example/transcribegenius/util/Constants.kt
+++ app/src/main/java/com/example/transcribegenius/util/Constants.kt
@@ -2,5 +2,3 @@ package com.example.transcribegenius.util
 
 
 
-const val YOUR_YT_API_KEY = "AIzaSyACERF2IDtOT33rkOa63LVn4o-ypZE8jQ0"
-const val CO_API_KEY = "eE9cViAYysfTuaHDbvAFJjp6xuW0zO4026RLrpEg"
